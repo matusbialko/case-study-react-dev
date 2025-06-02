24032d77b4d048fcfa334a33c9eceb9c3881f882
Matus B <bialkomatus@gmail.com>
2025-06-03 00:37:48 +0200

    open sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${openSans.className} font-[Arial] antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
