12751813477f40b561f87f98ebc9db424b45a3ba
Matus B <bialkomatus@gmail.com>
2025-06-03 16:28:25 +0200

    overlay loading, adjust style and spacing in auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,31 +1,64 @@
-import Image from "next/image";
+'use client'
+
+import Image from "next/image"
+import { usePathname } from "next/navigation"
+import React, { createContext, useContext, useState } from 'react'
+
+type LoadingContextType = {
+	loading: boolean
+	setLoading: (val: boolean) => void
+}
+
+const LoadingContext = createContext<LoadingContextType | undefined>(undefined)
+
+export const useLoading = () => {
+	return useContext(LoadingContext)
+}
 
 export default function AuthLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className="flex flex-col items-center">
-        <div className="w-full h-screen flex flex-col items-center">
-          <header className="w-full flex flex-col items-center">
-            <div className="w-full h-12 bg-black" />
-            <div className="login-container w-full h-35 flex flex-col justify-start items-start p-5">
-              <Image
-                  src="/GB-title.png"
-                  alt="GymBeam-title"
-                  width={265}
-                  height={75}
-              />
-            </div>
-          </header>
-          {children}
-        </div>
-        <footer className="w-full h-12 flex flex-row justify-center items-center bg-black text-white">
-          <p className="text-sm">© 2014 - 2025 GymBeam</p>
-        </footer>
-      </body>
-    </html>
-  );
+	const pathname = usePathname()
+
+	const [loading, setLoading] = useState(false)
+
+	return (
+		<div className="flex flex-col items-center">
+			<div className="w-full h-screen flex flex-col items-center bg-white md:bg-transparent">
+				<header className="w-full flex flex-col items-center">
+					<div className="w-full h-12 bg-black" />
+					<div className={`
+						${pathname == '/login' ? 'login-container' : 'register-container'}
+						w-full h-17.5 md:h-35 flex flex-col justify-start items-start p-5 pb-2.5 md:p-5
+					`}>
+						<Image
+							src="/GB-title.png"
+							alt="GymBeam-title"
+							width={265}
+							height={75}
+							className="w-35 h-10 md:w-[265px] md:h-[75px]"
+						/>
+					</div>
+				</header>
+				<LoadingContext.Provider value={{ loading, setLoading }}>
+					{loading && (
+					<div className="fixed top-0 left-0 w-screen h-screen flex flex-row justify-center items-center bg-white/30 z-50">
+						<Image
+							src="/loader.gif"
+							alt="loading"
+							width={256}
+							height={256}
+						/>
+					</div>
+					)}
+					{children}
+				</LoadingContext.Provider>
+			</div>
+			<footer className="w-full h-12 flex flex-row justify-center items-center bg-black text-white">
+				<p className="text-sm">2014 - 2025 GymBeam</p>
+			</footer>
+		</div>
+	)
 }
