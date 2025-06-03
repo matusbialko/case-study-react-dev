b7a5f7df561f8409461721cdb0e17a3753722d9f
Matus B <bialkomatus@gmail.com>
2025-06-03 16:26:36 +0200

    add required mark to input component

--- src/app/_components/input-field.tsx
+++ src/app/_components/input-field.tsx
@@ -3,26 +3,39 @@
 import React from 'react'
 
 interface InputFieldProps {
-  title: string
-  value: string | number
-  onChange: (value: string | number) => void
-  placeholder?: string
-  inputType: string
+	title: string
+	value: string | number
+	onChange: (value: string | number) => void
+	placeholder?: string
+	inputType: string,
+	requiredInput: boolean
 }
 
-const InputField: React.FC<InputFieldProps> = ({ title, value, onChange, placeholder, inputType = 'text' }) => {
-  return (
-    <div className="flex flex-col mt-4">
-      <label className="text-sm mb-[5px]">{title}</label>
-      <input
-        type={inputType}
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        placeholder={placeholder}
-        className="input-field"
-      />
-    </div>
-  )
+const InputField: React.FC<InputFieldProps> = ({
+	title,
+	value,
+	onChange,
+	placeholder,
+	inputType = 'text',
+	requiredInput
+}) => {
+  	return (
+		<div className="flex flex-col">
+			<label className="text-sm mb-[5px]">
+				{title}
+				{requiredInput && (
+					<span className="text-primary text-xs ml-1">*</span>
+				)}
+			</label>
+			<input
+				type={inputType}
+				value={value}
+				onChange={(e) => onChange(e.target.value)}
+				placeholder={placeholder}
+				className="input-field"
+			/>
+		</div>
+  	)
 }
 
 export default InputField
